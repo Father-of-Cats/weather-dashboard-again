40d34526564f293988bde4b888da7b1002faecf6
Hunter Nelson <huntknelson@gmail.com>
2022-04-23 22:09:11 -0500

    added for row elements, created event listener and handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// Form Elements
+const formEl = document.querySelector("#cityinput");
+const InputEl = document.querySelector("#cityname");
+const buttonEl = document.querySelector("#citysubmit");
+
+// Today's Weather Elements
+const headerEl = document.querySelector("#current-name");
+const containerEl = document.querySelector("current-container");
+
+
+// event handler for search submit button
+const submitEvent = (event) => {
+    event.preventDefault();
+    console.log("SUBMIT")
+}
+
+// listens for form submit
+formEl.addEventListener('submit',submitEvent);
+
 
 const getWeather = () => {
     let city = 'dallas';
@@ -10,4 +29,4 @@ const getWeather = () => {
         });
     })
 }
-getWeather();
+// getWeather();
