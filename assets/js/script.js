50cc30458c50404eff7d7b61a16a500deb79c86b
Hunter Nelson <huntknelson@gmail.com>
2022-04-24 02:07:47 -0500

    created list group of weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,34 @@ $(document).ready(function() {
     const APIKey = "eafa260ed54cac7951d9d21533a37bc8"
     //Jquery submit handler
     $("#cityform").submit((event)=>{
-        event.preventDefault();
-        const city = $("#cityname").val().trim();
+        event.preventDefault()
+        let city = $("#cityname").val().trim();
         if(city){ 
             getWeather(city);
             console.log(city);  
         } else {
             alert("Please enter a city name")
-        };
+        }
     });
 
+    getWeather = (city) => {
+        let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
 
+        fetch(queryURL).then((response) => {
+            response.json().then((data)=> {
+                dailyDisplay(data,city);
+            })
+        })
+    };
+    dailyDisplay = (data, city) => {
+        $("#current-container").html("")
+        $("#current-name").html(city).addClass('bg-light text-dark list-group text-center');
 
-// event handler for search submit button
-// const submitEvent = (event) => {
-//     event.preventDefault();
-//     console.log("SUBMIT")
-// }
+        // Appends weather data
+        $("#current-container").append(`<span class='list-group-item'>Temperature: ${data.main.temp}°F</span>`)
+        $("#current-container").append(`<span class='list-group-item'>Humidity: ${data.main.humidity}%</span>`)
+        $("#current-container").append(`<span class='list-group-item'>Wind Speed: ${data.wind.speed}MPH</span>`)
+        $("#current-container").append(`<span class='list-group-item'>Feels Like: ${data.main.feels_like}°F</span>`)
 
-// listens for form submit
-// formEl.addEventListener('submit',submitEvent);
-
-
-getWeather = (city) => {
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
-
-    fetch(queryURL).then((response) => {
-        response.json().then((data)=> {
-            console.log(data);
-        });
-    })
-}
+    }
 });
